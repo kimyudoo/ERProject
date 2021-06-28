c1e8c807543cfad743d7d6e14b02ef81123f9b9e
kimyudoo <kimyudoo@gmail.com>
2021-06-28 10:39:12 +0900

    소스코드 첫 업로드

---
+++ src/LoopTest.java
@@ -0,0 +1,29 @@
+
+public class LoopTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final int MAX = 1000;
+		int subMaxCount = 1;
+		int compareIndex = 0;
+		
+		for(int index = 0; index <= MAX; index++) {
+			if(index >= (int)Math.pow(10, subMaxCount)) {
+				subMaxCount++; 
+			}
+			for(int subIndex = 0; subIndex < subMaxCount; subIndex++) {
+				compareIndex = index / (int)Math.pow(10, subIndex);
+				if( (compareIndex % 10 == 3) || (compareIndex % 10 == 6) || (compareIndex % 10 == 9) ) {
+					System.out.print("(¦) ");
+				}				
+			}
+			System.out.printf("%d\n", index);
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
